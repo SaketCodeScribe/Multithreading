1ebf4d470243631bf6e516149b5cbb408fb01c6c
SaketCodeScribe <saket0565@gmail.coom>
2026-03-31 06:13:02 +0530

    Problem: FooBar Concepts - condition variable

--- src/problems/PrintFooBar.java
+++ src/problems/PrintFooBar.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PrintFooBar {
     public static void main(String[] args) throws InterruptedException {
@@ -12,10 +14,30 @@ public class PrintFooBar {
         System.out.println("PrintFooBar using flag");
         foobarWithFlag(executors);
         Thread.sleep(1000);
+
         System.out.println("\n\nPrintFooBar using Semaphores");
         foobarWithSemaphores(executors);
         Thread.sleep(1000);
+
+        System.out.println("\n\nPrintFooBar using condition variables");
+        foobarWithConditionVariable(executors);
         executors.shutdown();
+    }
+
+    private static void foobarWithConditionVariable(ExecutorService executors) {
+        FooBarWithWithConditionVariables fooBar = new FooBarWithWithConditionVariables();
+        int n = 100;
+
+        executors.submit(() -> {
+            for(int i=1; i<=n; i++){
+                fooBar.printFoo(() -> System.out.print("foo"));
+            }
+        });
+        executors.submit(() -> {
+            for(int i=1; i<=n; i++){
+                fooBar.printBar(() -> System.out.print("bar"));
+            }
+        });
     }
 
     private static void foobarWithSemaphores(ExecutorService executors) throws InterruptedException {
@@ -104,6 +126,46 @@ class FooBarWithSemaphores{
             finally{
                 fooPermit.release();
             }
+        }
+    }
+}
+
+class FooBarWithWithConditionVariables{
+    private ReentrantLock lock = new ReentrantLock();
+    private Condition foo = lock.newCondition();
+    private Condition bar = lock.newCondition();
+    private boolean barTurn = false;
+
+    public FooBarWithWithConditionVariables(){ }
+    public void printFoo(Runnable task){
+        lock.lock();
+        try{
+            while(barTurn){
+                foo.await();
+            }
+            task.run();
+            barTurn = true;
+            bar.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            lock.unlock();
+        }
+    }
+    public void printBar(Runnable task){
+        lock.lock();
+        try{
+            while(!barTurn){
+                bar.await();
+            }
+            task.run();
+            barTurn = false;
+            foo.signal();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            lock.unlock();
         }
     }
 }
