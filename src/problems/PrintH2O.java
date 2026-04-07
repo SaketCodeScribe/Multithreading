236fa5770e5df21c6e3067ea5b5959ee38aa6b31
SaketCodeScribe <saket0565@gmail.coom>
2026-04-07 07:18:40 +0530

    Problem: H2O - Barrier + Semaphores - Semaphores

--- src/problems/PrintH2O.java
+++ src/problems/PrintH2O.java
@@ -1,6 +1,8 @@
 package problems;
 
 import java.util.concurrent.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class PrintH2O {
     public static void main(String[] args) throws InterruptedException {
@@ -64,6 +66,49 @@ class H2OWithBarrier{
             oxygenPermit.acquire();
             barrier.await();
         } catch (BrokenBarrierException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+class H2OWithSemaphores{
+    private Semaphore hydrogenPermit = new Semaphore(0);
+    private Semaphore oxygenPermit = new Semaphore(0);
+    private Semaphore mutex = new Semaphore(1);
+    private int hydrogenCnt = 0;
+    private int oxygenCnt = 0;
+
+    public void hydrogen(Runnable task) throws InterruptedException {
+        mutex.acquire();
+        hydrogenCnt++;
+        try{
+            if (hydrogenCnt > 1 && oxygenCnt > 0){
+                hydrogenCnt -= 2;
+                hydrogenPermit.release(2);
+                oxygenCnt--;
+                oxygenPermit.release();
+            }
+            mutex.release();
+            hydrogenPermit.acquire();
+            task.run();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public void oxygen(Runnable task) throws InterruptedException {
+        mutex.acquire();
+        oxygenCnt++;
+        try{
+            if (hydrogenCnt > 1 && oxygenCnt > 0){
+                hydrogenCnt -= 2;
+                hydrogenPermit.release(2);
+                oxygenCnt--;
+                oxygenPermit.release();
+            }
+            mutex.release();
+            oxygenPermit.acquire();
+            task.run();
+        } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
     }
